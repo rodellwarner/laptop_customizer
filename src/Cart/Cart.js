d22e170045dccf16c10f26a54c83802c876cc5e7
Rodell Warner <rodell.warner@gmail.com>
2020-08-21 21:06:41 -0500

    Total component works

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Total from "../Total/Total";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -24,21 +25,11 @@ class Cart extends Component {
       );
     });
 
-    const total = Object.keys(this.props.selected).reduce(
-      (acc, curr) => acc + this.props.selected[curr].cost,
-      0
-    );
-
     return (
       <section className="main__summary">
         <h2>Your cart</h2>
         {summary}
-        <div className="summary__total">
-          <div className="summary__total__label">Total</div>
-          <div className="summary__total__value">
-            {USCurrencyFormat.format(total)}
-          </div>
-        </div>
+        <Total selected={this.props.selected} />
       </section>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>ELF Computing | Laptops</h1>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header>
+      <h1>ELF Computing | Laptops</h1>
+    </header>
+  );
 }
 
 export default Header;
