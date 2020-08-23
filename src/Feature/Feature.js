05e5a4eee7c63c371ee8f0c0ea7eea67ee0a4250
Rodell Warner <rodell.warner@gmail.com>
2020-08-23 15:22:10 -0500

    Feature-js component added

---
+++ src/Feature/Feature.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import slugify from "slugify";
+import FeatureItem from "../FeatureItem/FeatureItem";
+
+class Feature extends Component {
+  render() {
+    const labelForFeature = this.props.feature;
+    const featureHash = this.props.feature + "-" + this.props.index;
+    const options = this.props.features[this.props.feature].map((item) => {
+      const itemHash = slugify(JSON.stringify(item));
+      return (
+        <FeatureItem
+          key={itemHash}
+          item={item}
+          itemHash={itemHash}
+          feature={this.props.feature}
+          selected={this.props.selected}
+          handleUpdateFeature={this.props.handleUpdateFeature}
+        />
+      );
+    });
+
+    function FeatureLabel() {
+      return <h3>{labelForFeature}</h3>;
+    }
+
+    return (
+      <fieldset className="feature" key={featureHash}>
+        <legend className="feature__name">
+          <FeatureLabel />
+        </legend>
+        {options}
+      </fieldset>
+    );
+  }
+}
+
+export default Feature;
--- src/FeatureList/FeatureList.js
+++ src/FeatureList/FeatureList.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
-import slugify from "slugify";
-import FeatureItem from "../FeatureItem/FeatureItem";
+import Feature from "../Feature/Feature";
 
 class FeatureList extends Component {
   render() {
     const features = Object.keys(this.props.features).map((feature, index) => {
-      const featureHash = feature + "-" + index;
-      const options = this.props.features[feature].map((item) => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <FeatureItem
-            key={itemHash}
-            item={item}
-            itemHash={itemHash}
-            feature={feature}
-            selected={this.props.selected}
-            handleUpdateFeature={this.props.handleUpdateFeature}
-          />
-        );
-      });
-
-      function FeatureLabel() {
-        return <h3>{feature}</h3>;
-      }
-
+      const featureKey = "FK_" + feature;
       return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <FeatureLabel />
-          </legend>
-          {options}
-        </fieldset>
+        <Feature
+          key={featureKey}
+          feature={feature}
+          features={this.props.features}
+          index={index}
+          selected={this.props.selected}
+          handleUpdateFeature={this.props.handleUpdateFeature}
+        />
       );
     });
 
